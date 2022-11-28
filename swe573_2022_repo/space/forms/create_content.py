3e86f4db1092cbfefa15f8eee7f15f319d7c597e
Aydos Ilgazi <aydosilgazi@gmail.com>
2022-11-28 19:35:59 +0300

    spacedetails bug fix

--- swe573_2022_repo/space/forms/create_content.py
+++ swe573_2022_repo/space/forms/create_content.py
@@ -4,4 +4,4 @@ from space.models import ContentModel
 class CreateContentForm(forms.ModelForm):
     class Meta:
         model = ContentModel
-        fields= ('title',)
+        fields= ('title','link')

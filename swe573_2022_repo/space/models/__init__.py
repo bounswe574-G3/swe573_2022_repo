043f9c29b1a77dbcd753de6907d60d25381d2f93
Aydos Ilgazi <aydosilgazi@gmail.com>
2022-11-28 11:36:05 +0300

    content model added

--- swe573_2022_repo/space/models/__init__.py
+++ swe573_2022_repo/space/models/__init__.py
@@ -1,3 +1,5 @@
 from space.models.spacemodel import SpaceModel
 from .message import MessageModel
 from .step import StepModel
+from .content import ContentModel
+from .relation import RelationModel

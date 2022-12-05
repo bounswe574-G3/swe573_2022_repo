0774c264ef386465db7eb5d6492101adf41a60cd
Aydos Ilgazi <aydosilgazi@gmail.com>
2022-12-05 11:44:41 +0300

    annotation js added for displaying and creating annotation on page

--- swe573_2022_repo/colearning/urls.py
+++ swe573_2022_repo/colearning/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('space.urls')),
     path('quiz/', include('quiz.urls')),
+    path('',include('annotator.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

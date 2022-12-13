caa237655ed7b93779d3b806c840eddf1556ab2d
yildizmus <mustafayldz80@gmail.com>
2022-12-13 11:16:29 +0300

    bug has been fixed in the stepmodel form.

--- swe573_2022_repo/space/forms/create_step.py
+++ swe573_2022_repo/space/forms/create_step.py
@@ -4,10 +4,6 @@ from django.shortcuts import redirect, render, get_object_or_404
 
 
 class CreateStepModelForm(forms.ModelForm):
-    def __init__(self, stepspace, **kwargs):
-            super(CreateStepModelForm, self).__init__(**kwargs)
-            if stepspace:
-                self.fields['relatedresource'].queryset = StepModel.objects.filter(stepspace=stepspace)
     class Meta:
         model=StepModel
         fields= ('steptitle', 'stepcontent', 'relatedresource')
